90801769ed42321c982554f27dded2ef4b158117
LuisPulenta <luisalbertonu@gmail.com>
2023-06-08 19:19:24 -0300

    Ventanas modales

--- KPGeoData.WEB/Program.cs
+++ KPGeoData.WEB/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using CurrieTechnologies.Razor.SweetAlert2;
 using KPGeoData.WEB;
 using KPGeoData.WEB.Auth;
@@ -17,6 +18,7 @@ builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredModal();
 //builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 builder.Services.AddScoped<AuthenticationProviderJWT>();
